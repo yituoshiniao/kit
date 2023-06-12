977979dee2dd0c4daee91b76c2777d626998a02d
甘泉 <quanlong_gan@intsig.net>
2023-06-12 10:36:05 +0800

    edit

---
+++ xdb/v2/scopes.go
@@ -0,0 +1,11 @@
+package v2
+
+import "gorm.io/gorm"
+
+func NotDelete(db *gorm.DB) *gorm.DB {
+	return db.Where("is_del=0")
+}
+
+func Delete(db *gorm.DB) *gorm.DB {
+	return db.Where("is_del != 0")
+}
